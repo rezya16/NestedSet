96750dd142f64e58af800ad4089508f727885307
Dmitriy Reznichenko <dimon.fan.1998@gmail.com>
2020-01-22 23:21:13 +0200

    craete parent array and add opertation with lvl

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ switch ($argv[1]) {
         if (isset($argv[2])){
             $title = $argv[2];
         } else {
-            echo 'Error: 2nd argument must be a title';
+            return 'Error: 2nd argument must be a title';
         }
         if (isset($argv[3])){
             $parent_id = $argv[3];
@@ -28,17 +28,20 @@ switch ($argv[1]) {
             $parent_id = 1;
         }
 
-        $stmt = $pdo->prepare('SELECT rgt FROM category WHERE id = :id');
+
+        $stmt = $pdo->prepare('SELECT rgt,lvl FROM category WHERE id = :id');
         $stmt->execute(['id' => $parent_id]);
-        $parent_rgt = $stmt->fetchColumn();
+        $parent = $stmt->fetch(PDO::FETCH_ASSOC);
+
 
         $stmt = $pdo->prepare('UPDATE category SET rgt = rgt + 2 WHERE rgt >= :rgt');
-        $stmt->execute(['rgt' => $parent_rgt]);
+        $stmt->execute(['rgt' => $parent['rgt']]);
         $stmt = $pdo->prepare('UPDATE category SET lft = lft + 2 WHERE lft > :rgt');
-        $stmt->execute(['rgt' => $parent_rgt]);
+        $stmt->execute(['rgt' => $parent['rgt']]);
 
-        $stmt = $pdo->prepare('INSERT INTO category (title,lft,rgt) VALUES (:title, :lft, :rgt)');
-        $stmt->execute(['title' => $title, 'lft' => $parent_rgt, 'rgt' => ($parent_rgt + 1)]);
+        $stmt = $pdo->prepare('INSERT INTO category (title,lft,rgt,lvl) VALUES (:title, :lft, :rgt, :lvl)');
+        $stmt->execute(['title' => $title, 'lft' => $parent['rgt'], 'rgt' => $parent['rgt'] + 1, 'lvl' => $parent['lvl'] + 1]);
+
 
         break;
 }
