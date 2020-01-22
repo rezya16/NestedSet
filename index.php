12a70b1371e586160f641dd7e0480dabe8bb314e
Dmitriy Reznichenko <dimon.fan.1998@gmail.com>
2020-01-22 18:22:13 +0200

    add switch case

--- index.php
+++ index.php
@@ -14,3 +14,21 @@ $opt = [
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 $pdo = new PDO($dsn, $user, $pass, $opt);
+
+switch ($argv[1]) {
+    case 'addNode':
+        if (isset($argv[2])){
+            $title = $argv[2];
+        } else {
+            echo 'Error: 2nd argument must be a title';
+        }
+        if (isset($argv[3])){
+            $parent_id = $argv[3];
+        } else {
+            $parent_id = 1;
+        }
+
+
+
+        break;
+}
